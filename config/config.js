70a3a00a96d4d55f14fc278bb3635030785c2075
calebeLAR <ca6ele@gmail.com>
2023-04-10 13:37:39 -0400

    config.js

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "senha",
+    "database": "booksAPI_DEV",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "senha",
+    "database": "booksAPI_TESTE",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "senha",
+    "database": "booksAPI",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
