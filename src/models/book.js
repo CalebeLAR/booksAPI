f306ad85c9514528f1d54469a1d21af6b14d1d32
calebeLAR <ca6ele@gmail.com>
2023-04-11 11:30:17 -0400

    cria migrations e insere no banco de dados

--- src/models/book.js
+++ src/models/book.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   const Book = sequelize.define('Book', {
     id: DataTypes.INTEGER,
     title: DataTypes.STRING,
+    author: DataTypes.STRING,
     pageQuantity: DataTypes.INTEGER,
   });
 
