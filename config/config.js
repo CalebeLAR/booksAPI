0830cfcbb1a5b22916ab81520b0864e0e313034b
calebeLAR <ca6ele@gmail.com>
2023-04-11 11:04:22 -0400

    build

--- config/config.js
+++ config/config.js
@@ -1,23 +1,15 @@
+// src/config/config.js
+
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+};
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "senha",
-    "database": "booksAPI_DEV",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": "senha",
-    "database": "booksAPI_TESTE",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": "senha",
-    "database": "booksAPI",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  development: config,
+  test: config,
+  production: config,
+};
